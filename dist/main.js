ca1d4ade254cdc403ccd59dd7e503ed7703e4824
fitzchivalrymontrose <fitzchivalrymontrose@protonmail.com>
2022-01-28 03:30:24 -0500

    Created CSS file, linked with webpack.

--- dist/main.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";console.log("It's working for now...")})();
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 'use strict';
+import './style.css';
+
 
 console.log(`It's working for now...`);
+const content = document.querySelector('.content');
