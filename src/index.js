d502e128ab1df37cbbb9e33b92aad726a349d583
fitzchivalrymontrose <fitzchivalrymontrose@protonmail.com>
2022-01-28 04:08:45 -0500

    Set up image imports in webpack.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 'use strict';
 import './style.css';
 
-
 console.log(`It's working for now...`);
-const content = document.querySelector('.content');
 
+const content = document.createElement('div');
+content.classList.add('content');
 content.textContent = 'Is it visible?';
+
+document.body.appendChild(content);
